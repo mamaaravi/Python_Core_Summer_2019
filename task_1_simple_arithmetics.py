13154113668d985fe4ac2cd1bab6823ed631ba0d
Marta Kozak <48657601+mamaaravi@users.noreply.github.com>
2019-08-11 20:15:31 +0300

    add first HW

---
+++ task_1_simple_arithmetics.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a first nuber: "))
+b=int(input("Enter a second nuber: "))
+print('a+b = ', a+b, '\n')
+print('a-b = ',a-b, '\n')
+print('a*b = ',a*b, '\n')
+if b==0:
+    print('Error! Division by 0.\n')
+else:
+    print('a/b = ',a/b, '\n')
