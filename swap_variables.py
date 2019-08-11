afc463cb82fdb0a939bafb7f723c2e0966444eb9
Marta Kozak <ma.maaravi@gmail.com>
2019-08-11 22:43:59 +0300

    completed swap_var.py

---
+++ swap_variables.py
@@ -0,0 +1,12 @@
+var_1=input("Enter first variable: ")
+var_2=input("Enter second variable: ")
+var_1, var_2=var_2, var_1
+print("Swapped:\nFirst variable: ", var_1, "\nSecond variable: ", var_2)
+
+# Output sample:
+
+# Enter first variable: 5
+# Enter second variable: g
+# Swapped:
+# First variable:  g
+# Second variable:  5
